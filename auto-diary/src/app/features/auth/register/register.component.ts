a9b3d88dfa2c6e69ef1b1126d44b6baa845e810b
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-07 23:54:21 +0300

    feat: Add registerForm logic and HTML with reactive forms

--- auto-diary/src/app/features/auth/register/register.component.ts
+++ auto-diary/src/app/features/auth/register/register.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { UserRegister } from '../../../shared/interfaces/user';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    tel: new FormControl('', [Validators.required])
+  })
+
+  onSubmit() {
+    this.authService.register(this.registerForm.value as UserRegister).subscribe({
+      next: () => { this.router.navigate(['/home']) }
+    })
+  }
 
 }
