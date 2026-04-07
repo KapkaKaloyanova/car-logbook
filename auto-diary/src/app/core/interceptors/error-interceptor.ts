7624765af8ca42203f3da51082ba87175ed9f032
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-07 03:48:01 +0300

    refactor: enhance error interceptor with auth-aware handling
    Add specific handling for 401 Unauthorized errors to distinguish between login failures and expired sessions.
    Implemented automatic session cleanup and нажигате to login on 401 (for non-auth endpoints).
    Add support for 409 Conflict (e.g., existing user)

--- auto-diary/src/app/core/interceptors/error-interceptor.ts
+++ auto-diary/src/app/core/interceptors/error-interceptor.ts
@@ -2,31 +2,51 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { NotificationService } from '../services/notification.service';
 import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+const AUTH_ENDPOINTS = ['/users/login', '/users/register'];
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const notifService = inject(NotificationService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
-      let message = 'There is some error';
+      let message = 'Unexpected error occured';
 
-      if (err.status === 0) {
-        message = 'Can not connect to the server'
-      } else if (err.error?.message) {
-        message = err.error.message
+      if (err.error instanceof ErrorEvent) {
+        message = err.error.message; // client-side error network or browser
+      } else if (err.status === 0) {
+        message = 'Can not connect to the server. Check your internet connection';
+      } else if (err.status === 400) {
+        message = err.error?.message || 'Bad request';
       } else if (err.status === 401) {
-        message = 'Unauthorized access';
+        // Проверяваме дали текущият URL съдържа някой от нашите Auth пътища
+        const isAuthRequest = AUTH_ENDPOINTS.some(endpoint => req.url.includes(endpoint));
+        if (isAuthRequest) {
+          message = 'Invalid email or password';
+        } else {
+          message = 'Session expired. Please log in again';
+          authService.clearSession();
+          router.navigate(['/login']);
+        }
       } else if (err.status === 403) {
-        message = 'Forbidden';
+        message = 'Forbidden. You do not have permission for this action';
       } else if (err.status === 404) {
         message = 'Resource not found';
+      } else if (err.status === 409) {
+        message = 'This email or username is already taken';
       } else if (err.status >= 500) {
-        message = 'Server error! Try again later';
+        message = 'Internal server error! Try again later';
+      } else if (err.error?.message) {// за всички останали случаи, когато сървъра праща грешка
+        message = err.error.message || `Error: ${err.status}`;
       }
 
       notifService.error(message);
+
       return throwError(() => err);
     })
-
   );
 };
