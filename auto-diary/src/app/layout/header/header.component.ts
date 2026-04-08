4876d5724a0619aeff00385dca6f63d8035773a6
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-09 02:07:59 +0300

    feat(header): implement dynamic car dropdown and fix visibility
    Fixed dropdown visibility by removing display: none from CSS.
    Added click event for toggling the car list.
    Integrated computed username from AuthService.
    Added mock data for user cars for UI testing.

--- auto-diary/src/app/layout/header/header.component.ts
+++ auto-diary/src/app/layout/header/header.component.ts
@@ -1,20 +1,37 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
-import { CarAddDocumentComponent } from "../../features/cars/car-add-document/car-add-document.component";
-
+// import { CarService } from '../../core/services/car.service'; // Бъдещия carService
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, CarAddDocumentComponent],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  private router = inject(Router);
-  private authService = inject(AuthService);
+   router = inject(Router);
+   authService = inject(AuthService);
+  // carService = inject(CarService); // Тук ще взимаме колите
 
   isLoggedIn = this.authService.isLoggedIn;
   username = computed(() => this.authService.currentUser()?.username ?? '');
+
+  isDropdownOpen = signal(false);
+
+  // Засега правим примерен списък, докато вържем бекенда
+  userCars = signal([
+    { id: '1', brand: 'BMW', model: '320d' },
+    { id: '2', brand: 'Opel', model: 'Astra' }
+  ]);
+
+  toggleDropdown() {
+    this.isDropdownOpen.update(val => !val);
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen.set(false);
+  }
+
   logout() {
     this.authService.logout().subscribe({
       next: () => { this.router.navigate(['/home']) }
