f83a420a674fa3c6188e9ab064ffd9075f552e53
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-09 03:44:49 +0300

    feat(update) : update CSS for login and register

--- auto-diary/src/app/features/auth/login/login.component.ts
+++ auto-diary/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../../core/services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { UserLogin } from '../../../shared/interfaces/user';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
--- auto-diary/src/app/features/auth/register/register.component.ts
+++ auto-diary/src/app/features/auth/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from '../../../core/services/auth.service';
-import { Router } from '@angular/router';
+import { Router , RouterLink} from '@angular/router';
 import { UserRegister } from '../../../shared/interfaces/user';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -17,12 +17,26 @@ export class RegisterComponent {
   registerForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
+    tel: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    tel: new FormControl('', [Validators.required])
-  })
+    rePassword: new FormControl('', [Validators.required]),
+  },{
+    validators: this.passwordMatchValidator
+  });
 
+  // Валидатор за съвпадение на паролите
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const rePassword = control.get('rePassword')?.value;
+    return password === rePassword ? null : { passwordMismatch: true };
+  }
   onSubmit() {
-    this.authService.register(this.registerForm.value as UserRegister).subscribe({
+    if(this.registerForm.invalid) { return; }
+    
+    // Премахване на полето rePassword за да не се изпраща към authService
+    const { rePassword, ...registerData } = this.registerForm.value;
+
+    this.authService.register(registerData as UserRegister).subscribe({
       next: () => { this.router.navigate(['/home']) }
     })
   }
