77dfa66a6b221f216505bc6011e2facaba8f8fe0
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-11 02:23:23 +0300

    feat: Add initial structure for CarDetailComponent

--- auto-diary/src/app/features/cars/car-details/car-details.component.ts
+++ auto-diary/src/app/features/cars/car-details/car-details.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { CarService } from '../../../core/services/car.service';
+import { Car } from '../../../shared/interfaces/car';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-car-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './car-details.component.html',
   styleUrl: './car-details.component.css',
 })
-export class CarDetailsComponent {
+export class CarDetailsComponent implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  private carService = inject(CarService);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  currentCar = signal<Car | null>(null);
+
+  isOwner = computed(() => {
+    return this.currentCar()?._ownerId === this.authService.currentUser()?._id
+  })
+
+  ngOnInit(): void {
+
+    const carId = this.activatedRoute.snapshot.params['id'];
+
+    if (carId) {
+      this.carService.getCarById(carId)
+        .subscribe({
+          next: car => this.currentCar.set(car)
+        });
+    }
+  }
+
+  onDelete(carId: string) {
+    this.carService.deleteCar(carId).subscribe({
+      next: () => {
+        this.currentCar.set(null);
+        this.router.navigate(['/cars']);
+      }
+    });
+  }
 }
