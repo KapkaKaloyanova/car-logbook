34be836d09a64135e5c50f0ab13ac3b1d641d9c7
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-16 00:48:41 +0300

    refactor: change the names of the service service and methods to use serviceRecord

--- auto-diary/src/app/core/services/service-record.service.spec.ts
+++ auto-diary/src/app/core/services/service-record.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CarServiceRecordService } from './car-service-record.service';
+import { ServiceRecordService } from './service-record.service';
 
 describe('CarServiceRecordService', () => {
-  let service: CarServiceRecordService;
+  let service: ServiceRecordService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CarServiceRecordService);
+    service = TestBed.inject(ServiceRecordService);
   });
 
   it('should be created', () => {
--- auto-diary/src/app/core/services/service-record.service.ts
+++ auto-diary/src/app/core/services/service-record.service.ts
@@ -6,28 +6,28 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class CarServiceRecordService {
+export class ServiceRecordService {
   private httpClient = inject(HttpClient);
   private apiUrl = 'http://localhost:3030/data/service-records';
 
-  createCarServiceRecord(serviceData: Partial<ServiceRecord>): Observable<ServiceRecord> {
+  createServiceRecord(serviceData: Partial<ServiceRecord>): Observable<ServiceRecord> {
     return this.httpClient.post<ServiceRecord>(this.apiUrl, serviceData);
    }
 
-  getCarServiceRecordsById(carId:string): Observable<ServiceRecord[]> { 
+  getServiceRecordsById(carId:string): Observable<ServiceRecord[]> { 
     return this.httpClient.get<ServiceRecord[]>(`${this.apiUrl}?where=carId%3D%22${carId}%22`);
   }
 
-  getCarServiceRecordById(serviceId: string): Observable<ServiceRecord> { 
+  getServiceRecordById(serviceId: string): Observable<ServiceRecord> { 
     return this.httpClient.get<ServiceRecord>(`${this.apiUrl}/${serviceId}`);
   }
 
-  deleteCarServiceRecord(serviceId: string): Observable<void> {
+  deleteServiceRecord(serviceId: string): Observable<void> {
     return this.httpClient.delete<void>(`${this.apiUrl}/${serviceId}`)
    }
 
-  editCarServiceRecord(serviceId: string, serviceData: Partial<ServiceRecord>): Observable<ServiceRecord> {
-    return this.httpClient.put<ServiceRecord>(`${this.apiUrl}/${serviceId}`, serviceData)
+  editServiceRecord(serviceId: string, serviceRecordData: Partial<ServiceRecord>): Observable<ServiceRecord> {
+    return this.httpClient.put<ServiceRecord>(`${this.apiUrl}/${serviceId}`, serviceRecordData)
    }
 
 
