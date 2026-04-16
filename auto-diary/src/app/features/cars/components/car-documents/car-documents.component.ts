226f33f7ee727d7308e253011896e31d8cfec744
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-17 00:25:03 +0300

    feat: add CarDocumentsComponent with cards layout and documentType pipe

--- auto-diary/src/app/features/cars/components/car-documents/car-documents.component.ts
+++ auto-diary/src/app/features/cars/components/car-documents/car-documents.component.ts
@@ -1,14 +1,40 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { Car } from '../../../../shared/interfaces/car';
+import { DocumentRecordService } from '../../../../core/services/document-record.service';
+import { DocumentRecord } from '../../../../shared/interfaces/document-record';
+import { RouterLink } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+import { DocumentTypePipe } from '../../../../shared/pipes/document-type.pipe';
 
 @Component({
   selector: 'app-car-documents',
-  imports: [],
+  imports: [RouterLink, DecimalPipe, DocumentTypePipe],
   templateUrl: './car-documents.component.html',
   styleUrl: './car-documents.component.css',
 })
-export class CarDocumentsComponent {
-    car = input.required<Car>();
+export class CarDocumentsComponent implements OnInit {
+  private documentRecordService = inject(DocumentRecordService);
+  car = input.required<Car>();
 
+  documentRecords = signal<DocumentRecord[]>([]);
 
+  ngOnInit(): void {
+    this.documentRecordService.getDocumentRecordsById(this.car()._id).subscribe({
+      next: (documentRecord) => {
+        this.documentRecords.set(documentRecord)
+      },
+      error: (err) => console.error('Грешка при получаване на записите:', err)
+    })
+  }
+
+  onDelete(documentRecorddId: string){
+    if(confirm('Сигурни ли сте, че искате да изтриете този документ?')) {
+      this.documentRecordService.deleteDocumentRecord(documentRecorddId).subscribe({
+        next: () => {
+          this.documentRecords.update(rec => rec.filter(r => r._id !== documentRecorddId));
+        },
+        error: (err) => console.error('Грешка при изтриване на запис:', err)
+      })
+    }
+  }
 }
