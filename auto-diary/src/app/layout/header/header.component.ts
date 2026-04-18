e23e2fbede09ea82a82971ce70b10d39e88b85eb
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2026-04-18 13:25:37 +0300

    feat: add notification bell number icon for count of allerts and toggle alerts

--- auto-diary/src/app/layout/header/header.component.ts
+++ auto-diary/src/app/layout/header/header.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../core/services/auth.service';
 import { CarService } from '../../core/services/car.service';
 import { Car } from '../../shared/interfaces/car';
 import { Subscription } from 'rxjs';
+import { AlertService } from '../../core/services/alert.service';
 @Component({
   selector: 'app-header',
   imports: [RouterLink, RouterLinkActive],
@@ -14,23 +15,32 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private router = inject(Router);
   private authService = inject(AuthService);
   private carService = inject(CarService);
+  private alertService = inject(AlertService);
 
   isLoggedIn = this.authService.isLoggedIn;
   username = computed(() => this.authService.currentUser()?.username ?? '');
+  alerts = signal<string[]>([]);
 
   isDropdownOpen = signal(false);
+  isAlertsOpen = signal(false);
 
   userCars = signal<Car[]>([]);
   private subscription!: Subscription;
 
 
+
   toggleDropdown() {
     this.isDropdownOpen.update(val => !val);
+  }
+  alertToggleDropdown() {
+    this.isAlertsOpen.update(val => !val);
   }
 
   closeDropdown() {
     this.isDropdownOpen.set(false);
   }
+  alertCloseDropdown() { 
+    this.isAlertsOpen.set(false); } 
 
   logout() {
     this.authService.logout().subscribe({
@@ -40,17 +50,33 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     // Абониране за router events и презареждане на userCars след всяка навигация
-    this.subscription = this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        if (this.authService.isLoggedIn()) {
-          this.carService.getCarsByOwner(this.authService.currentUser()!._id)
-            .subscribe(cars => this.userCars.set(cars));
-        }
-      }
-    })
+    this.subscription = this.router.events.subscribe(
+      {
+        next: event => {
+          if (event instanceof NavigationEnd) {
+            if (this.authService.isLoggedIn()) {
+              this.carService.getCarsByOwner(this.authService.currentUser()!._id).subscribe(
+                {
+                  next: cars => {
+                    this.userCars.set(cars);
+                    this.alertService.getAlertsForAllCars(cars).subscribe(
+                      {
+                        next: alerts => this.alerts.set(alerts),
+                        error: (err) => console.error('Грешка при получаване на известията:', err)
+                      }
+                    );
+                  },
+                  error: (err) => console.error('Грешка при получаване на автомобилите:', err)
+                });
+            };
+          }
+        },
+        error: (err) => console.error('Грешка при получаване на router events:', err)
+      });
   }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
-  }
+  };
 
 }
